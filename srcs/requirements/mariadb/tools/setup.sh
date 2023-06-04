8695c7f49b3527e42ee35521e52a7f4f37b53422
chaejin-jen <jen71324@gmail.com>
2023-06-05 07:43:36 +0900

    🐛 FIX: test passed

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-chown -R mysql:mysql /var/lib/mysql;
+service mysql start
 
-service mysql start;
-
-if ! [ -f "/var/lib/mysql/.mysql.setup.ok" ]; then 
-    mysql -hlocalhost -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
-    mysql -hlocalhost -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
-    mysql -hlocalhost -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
-    mysql -hlocalhost -e "FLUSH PRIVILEGES";
-    mysql -hlocalhost -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
-    # mysql -hlocalhost -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('$MYSQL_ROOT_PASSWORD')"    
-    mysql -hlocalhost -e "FLUSH PRIVILEGES";
-
-    mysql -hlocalhost $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD;
-    mysqladmin -hlocalhost -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown;
-    touch /var/lib/mysql/.mysql.setup.ok;
+if [ ! -e $/var/lib/mysql/.mysql.setup.ok ]; then
+    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
+    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
+    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
+    mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+    mysql -e "FLUSH PRIVILEGES";
+    mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
+    touch /var/lib/mysql/.mysql.setup.ok
 fi
+mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
 
-exec mysqld;
+exec mysqld
