ed67921f0e68335196a61c5c18864e7dbe32743c
chaejin-jen <jen71324@gmail.com>
2023-06-05 15:41:48 +0900

    ♻️REFACTOR: makefile, comment, non-install mariadb-client in maridb container

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,18 +4,18 @@ sleep 5
 
 until mariadb -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD 2> /dev/null ; do
     echo "waiting for MariaDB to be on..."
-    sleep 2
+    sleep 4
 done
 
-echo "check wp installed..."
 # wp install, add defalut user, new user
+echo "check wp installed..."
 if ! [ -f "/var/www/html/wordpress/index.php" ]; then 
     wp core download --locale=en_US && \
     wp config create --dbname=$MYSQL_DATABASE --dbhost=$MYSQL_HOST --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD && \
     wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL && \
-    wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS && \
-    echo "wp setup!"
+    wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS
 fi
+echo "wp setup!"
 
 chown -R www-data:www-data /var/www/html
 
