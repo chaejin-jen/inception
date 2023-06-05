3c99b855a34cb5b127caa8df409d294984605f2d
chaejin-jen <jen71324@gmail.com>
2023-06-05 15:39:33 +0900

    🐛 FIX: mariadb 1045, 2002 ERROR

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-service mysql start
+chown -R mysql:mysql /var/lib/mysql
 
-if [ ! -e $/var/lib/mysql/.mysql.setup.ok ]; then
-    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
-    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
-    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
-    mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-    mysql -e "FLUSH PRIVILEGES";
-    mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
+if ! [ -f /var/lib/mysql/.mysql.setup.ok ]; then 
+    echo "mysql setup"
+	service mysql start
+    
+    # init directory
+    mysql_install_db --user=mysql --skip-test-db --skip-log-bin --datadir=/var/lib/mysql > /dev/null
+    
+    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; \
+    CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; \
+    FLUSH PRIVILEGES;";
+
+    mysql -u$MYSQL_ROOT -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES";
+    
+    # Execute the client to handle root password
+    # mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
+    mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
     touch /var/lib/mysql/.mysql.setup.ok
 fi
-mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
 
-exec mysqld
+mysqld --user=mysql
